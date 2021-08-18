c1e470fab97b98e94d8b284ec878af1121b72f19
ElenaRubtsova <elena.rub2000@gmail.com>
2021-08-18 13:54:29 +0300

    [ex1-6] Закрыть для неавторизованных пользователей доступ к странице Миссия и стратегия (/company/mission.php).

---
+++ .top.menu.php
@@ -0,0 +1,60 @@
+<?
+$aMenuLinks = Array(
+	Array(
+		"Компания", 
+		"company/", 
+		Array(), 
+		Array("MENU_TEXT"=>"текст для пункта Компания"), 
+		"" 
+	),
+	Array(
+		"Новости", 
+		"news/", 
+		Array(), 
+		Array("MENU_COLOR"=>"color-red"), 
+		"" 
+	),
+	Array(
+		"Продукция", 
+		"products/", 
+		Array(), 
+		Array("MENU_COLOR"=>"color-green"), 
+		"" 
+	),
+	Array(
+		"Услуги", 
+		"services/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Партнерам", 
+		"/partners/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Отзывы", 
+		"/rew/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Поиск", 
+		"/search/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Опросы", 
+		"/ask/", 
+		Array(), 
+		Array(), 
+		"" 
+	)
+);
+?>
--- local/templates/exam1/footer.php
+++ local/templates/exam1/footer.php
@@ -14,23 +14,24 @@ use Bitrix\Main\Localization\Loc;
     <!-- side menu -->
     <? if (!$bIsMain): ?>
         <? $APPLICATION->IncludeComponent(
-            "bitrix:menu",
-            "left",
-            array(
-                "ALLOW_MULTI_SELECT" => "N",
-                "CHILD_MENU_TYPE" => "left",
-                "DELAY" => "N",
-                "MAX_LEVEL" => "1",
-                "MENU_CACHE_GET_VARS" => array(),
-                "MENU_CACHE_TIME" => "3600",
-                "MENU_CACHE_TYPE" => "A",
-                "MENU_CACHE_USE_GROUPS" => "Y",
-                "ROOT_MENU_TYPE" => "left",
-                "USE_EXT" => "Y",
-                "COMPONENT_TEMPLATE" => "left"
-            ),
-            false
-        ); ?>
+	"bitrix:menu", 
+	"left", 
+	array(
+		"ALLOW_MULTI_SELECT" => "N",
+		"CHILD_MENU_TYPE" => "left",
+		"DELAY" => "N",
+		"MAX_LEVEL" => "2",
+		"MENU_CACHE_GET_VARS" => array(
+		),
+		"MENU_CACHE_TIME" => "3600",
+		"MENU_CACHE_TYPE" => "A",
+		"MENU_CACHE_USE_GROUPS" => "Y",
+		"ROOT_MENU_TYPE" => "left",
+		"USE_EXT" => "Y",
+		"COMPONENT_TEMPLATE" => "left"
+	),
+	false
+); ?>
     <? endif; ?>
     <!-- /side menu -->
 
