ca979c7541a58a22724ece92eedf0b1dc4639e7f
ElenaRubtsova <elena.rub2000@gmail.com>
2021-08-18 16:57:11 +0300

    [ex1-12] Ускорение работы с контентом: html заготовка с контента inner.html

---
+++ local/templates/exam1/snippets/.content.php
@@ -0,0 +1,5 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
+<?
+$SNIPPETS = Array();
+$SNIPPETS['snippet0001.snp'] = Array('title' => 'inner.html', 'description' => 'Заготовка с контента inner.html');
+?>
